a714d6da22df8dc680d655599ca23df91a891f2f
ShiPinSigSrv <zhaoqi256@sina.com>
2016-12-22 20:52:49 +0800

    main-1

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 int main(){
-	string src_path = "G:\\img\\1.jpg";
-	string tar_path = "G:\\img\\1gray.jpg";
-	Gray(src_path, tar_path, 2);
+	string src_path = "E:\\img\\1.JPG";
+	string tar_path = "E:\\img\\1gray.JPG";
+	Gray(src_path, tar_path, 5);
 	system("pause");
 	return 0;
 }
