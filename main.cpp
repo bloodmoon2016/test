768562ce8b6d5a9eb8aab79a1d6fef36a659a7e1
ShiPinSigSrv <zhaoqi256@sina.com>
2017-01-04 18:03:04 +0800

    bug correct-1

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@ using namespace std;
 int main(){
 	string src_path = "E:\\img\\1.JPG";
 	string tar_path = "E:\\img\\1gray.JPG";
-	Gray(src_path, tar_path, 5);
+//	Gray(src_path, tar_path, 2);
+	Threshold(src_path, tar_path, 128, 256, 0);
 	system("pause");
 	return 0;
 }
